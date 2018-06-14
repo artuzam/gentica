bca78402550b182063c660f6eb78cf9c28ccd220
Carlos Zamora Cerdas <b37709@ecci.ucr.ac.cr>
2018-06-14 12:34:45 -0600

    Nada

--- arboles.cpp
+++ arboles.cpp
@@ -9,3 +9,17 @@ arboles::~arboles()
 {
     //dtor
 }
+
+arboles::int[] crearArbol(int maxDepth){
+	char oper[] = {'+','-','/','*'};
+	double term[] = {-1,-2,-3,-4,-5,-6,-7,-8,-9,1,2,3,4,5,6,7,8,9,-0.1,-0.01,-0.001,0.1,0.01,0.001};
+	
+	
+	int arbol[31];
+	
+}
+
+arboles::int randPos(int largo){
+	srand (time(NULL));
+	int indice = rand() % largo;
+}
--- arboles.h
+++ arboles.h
@@ -1,12 +1,17 @@
 #ifndef ARBOLES_H
 #define ARBOLES_H
 
+#include <stdio.h>      /* printf, scanf, puts, NULL */
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
 
 class arboles
 {
     public:
         arboles();
         virtual ~arboles();
+		int[] crearArbol(int maxDepth);
+		int randomPos(int largo);
 
     protected:
 
